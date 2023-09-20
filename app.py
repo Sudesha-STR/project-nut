d85e6d510748aa4a939bf0601368910cf9658d10
Sudesha T R <104151479+Sudesha-STR@users.noreply.github.com>
2023-09-20 20:24:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,9 +74,9 @@ def main():
 
             # Load the MobilenetV2 models
             model_urls = [
-                "https://github.com/anivenk25/project-areca/blob/main/mobilenetv2_model.h5",
-                "https://github.com/anivenk25/project-areca/blob/main/custom_model_2.h5",
-                "https://github.com/anivenk25/project-areca/blob/main/custom_model.h5",
+                r"mobilenetv2_model.h5",
+                r"custom_model_2.h5",
+                r"custom_model.h5",
             ]
 
             model_paths = []
